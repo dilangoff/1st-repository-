f32c1b22cbfdd4ad0dde27542bdbdf0ad09a6de9
Dilan Goff <02dilan.goff@gulllakecs.org>
2019-02-27 10:00:34 -0500

    add funcitions & faribles pt. 3

---
+++ Funcitions-GIT.py
@@ -0,0 +1,35 @@
+#4.13.3: Greeting
+#Dilan Goff
+#2.6.19
+
+'''
+nmae = input("what is your name: ")
+
+def greeting():
+    pirnt ("hi there " + name + "!")
+    Print("Nice to meet ypu!")
+
+greeting()
+'''
+#4.13.4: Funtions and varibles
+#Dilan Goff
+#2.14.19
+'''
+x = 406
+
+def print_somthing():
+    x = 13
+    print (x)
+
+print_somthing()
+print(x)
+'''
+
+#4.13.5: funcitions and varibles part 2
+#Dilan Goff
+#2.14.19
+
+def something():
+    print(my_variable + 10)
+
+something()
